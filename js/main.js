a19e7323773d63d768a7d2f6fa3aacc5855f46d2
shyamsha <shyamkumar97@live.com>
2019-03-21 23:14:19 +0530

    intial commit

--- js/main.js
+++ js/main.js
@@ -135,12 +135,12 @@ $(document).ready(function() {
 			removalDelay: 300,
 			mainClass: "mfp-with-zoom",
 			gallery: {
-				enabled: true
+				enabled: false
 			},
 			zoom: {
 				enabled: true, // By default it's false, so don't forget to enable it
 
-				duration: 300, // duration of the effect, in milliseconds
+				duration: 500, // duration of the effect, in milliseconds
 				easing: "ease-in-out", // CSS transition easing function
 
 				// The "opener" function should return the element from which popup will be zoomed in
